81237f715765536e439e7a8a55f13321f9bc5148
JosuePerez <a6523150022@utch.edu.mx>
2024-08-21 00:21:58 -0600

    Add the props at the moment of create the tree.

--- src/ArbolDeNavidad.java
+++ src/ArbolDeNavidad.java
@@ -1,23 +1,26 @@
-public class ArbolDeNavidad {
-    static String tree;
-    static int height = 4;
-    static String[] prop = {"@", ""}; //Falta poner los adornos
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class ArbolDeNavidad {
+    static String tree; //String that shows the result in console
+    static int height = 4; //Specifies the tree's height
+    static String[] prop = {"@", "*", "◯"}; //Props that are going to use in tree
+    static Random random = new Random(); //Instantiate of random to create random numbers
+
+    public static void main(String[] args) {
         tree = GenerarArbol(height);
         System.out.println(tree);
-
-
     }
 
     public static String GenerarArbol(int height){
         String trees = "";
+        int p;
 
         for(int i = 1; i <= height; i++){
             trees = trees + GenerarEspacios(height - i);
+            p = random.nextInt(2);
 
             for (int j = 0; j < i; j++){
-                trees = trees  + " " + "*";
+                trees = trees  + " " + prop[p];
             }
             trees = trees + "\n";
         }
